b6f94002afec11f6994f93ef9ae0ec10047eec4e
milad <miladjalali1384@gmail.com>
2022-03-11 18:24:15 +0800

    4

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿
+            int number;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("just number is mmoh");
+            number=Convert.ToInt32(Console.ReadLine());
+            Console.ResetColor();
+            
+            switch(number)
+            {
+                case 1 :
+
+                    Console.WriteLine("January");
+                    break;
+
+                case 2:
+
+                    Console.WriteLine("February");
+                    break;
+                case 3:
+
+                    Console.WriteLine("March");
+                    break;
+                case 4:
+
+                    Console.WriteLine("April");
+                    break;
+                case 5:
+
+                    Console.WriteLine("May");
+                    break;
+                case 6:
+
+                    Console.WriteLine("June");
+                    break;
+                case 7:
+
+                    Console.WriteLine("July");
+                    break;
+                case 8:
+
+                    Console.WriteLine("August");
+                    break;
+                case 9:
+
+                    Console.WriteLine("September");
+                    break;
+                case 10:
+
+                    Console.WriteLine("October");
+                    break;
+                case 11:
+
+                    Console.WriteLine("November");
+                    break;
+                case 12:
+
+                    Console.WriteLine("December");
+                    break;
+
+                default:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("nist");
+                    break;
+
+                   
+
+            }
+ Console.ReadKey();
